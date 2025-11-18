375b0977056258ac8e19f2d248a066f01736999d
Blue-Phoenix82 <sumangoudafiles@gmail.com>
2025-11-18 10:50:18 +0530

    prof

--- routesandlayouts/App.js
+++ routesandlayouts/App.js
@@ -18,3 +18,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+import React from "react";
+
+
+function App() {
+  return React.createElement("div", null, React.createElement(profile));
+}
+
+
+
